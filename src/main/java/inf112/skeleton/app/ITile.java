4044e2399189429d2417dd1b3f610b85ad5a3c2e
BenDadger <Benjamin140111@gmail.com>
2019-02-12 15:20:52 +0100

    Added empty tile interface

---
+++ src/main/java/inf112/skeleton/app/ITile.java
@@ -0,0 +1,12 @@
+package inf112.skeleton.app;
+
+public interface ITile {
+    //TODO make interface
+    /*
+    ITile is the interface for the tiles on the board, a tile can be empty, have walls north, west, east or north,
+    have a wrench, have a cog, have a moving conveyor belt, a wall or a hole and this information should be accessible.
+
+    The tile doesn't for the time being need to know where it is or what's around it, it's coordinated are being
+    kept by the board.
+     */
+}
