52f567c5180876242f45951300b6062419a2cbb8
BenDadger <Benjamin140111@gmail.com>
2019-02-19 14:37:01 +0100

    Made basic tile interface

--- src/main/java/inf112/skeleton/app/ITile.java
+++ src/main/java/inf112/skeleton/app/ITile.java
@@ -1,7 +1,6 @@
 package inf112.skeleton.app;
 
 public interface ITile {
-    //TODO make interface
     /*
     ITile is the interface for the tiles on the board, a tile can be empty, have walls north, west, east or north,
     have a wrench, have a cog, have a moving conveyor belt, a wall or a hole and this information should be accessible.
@@ -9,4 +8,17 @@ public interface ITile {
     The tile doesn't for the time being need to know where it is or what's around it, it's coordinated are being
     kept by the board.
      */
+
+    /**
+     * Checks if there is a wall on a specified side of the tile.
+     *
+     * @param direction the side of the tile to check
+     * @return true if there is a wall on the tile in the specified direction
+     */
+    boolean hasWall(Facing direction);
+
+    /**
+     * @return true if the tile is a hole
+     */
+    boolean isHole();
 }
