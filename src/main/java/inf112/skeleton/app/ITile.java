f996b7cf3c646bdb6b4208e8a560e7e4e442dfdf
BenDadger <Benjamin140111@gmail.com>
2019-02-19 15:47:22 +0100

    Fixed minor typo on a comment

--- src/main/java/inf112/skeleton/app/ITile.java
+++ src/main/java/inf112/skeleton/app/ITile.java
@@ -5,7 +5,7 @@ public interface ITile {
     ITile is the interface for the tiles on the board, a tile can be empty, have walls north, west, east or north,
     have a wrench, have a cog, have a moving conveyor belt, a wall or a hole and this information should be accessible.
 
-    The tile doesn't for the time being need to know where it is or what's around it, it's coordinated are being
+    The tile doesn't for the time being need to know where it is or what's around it, it's coordinates are being
     kept by the board.
      */
 
