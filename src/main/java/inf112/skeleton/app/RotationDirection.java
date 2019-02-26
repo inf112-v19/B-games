4d123e4368b50846b8ee4a2d76364ba8208eff27
Jonas Bjørke Opsahl <jop004@uib.no>
2019-02-26 14:34:20 +0100

    Enum rotation direction added

---
+++ src/main/java/inf112/skeleton/app/RotationDirection.java
@@ -0,0 +1,9 @@
+package inf112.skeleton.app;
+
+/**
+ * Direction of rotation for cogwheels (clockwise or counterclockwise)
+ */
+public enum RotationDirection {
+    CLOCKWISE,
+    COUNTERCLOCKWISE
+}
