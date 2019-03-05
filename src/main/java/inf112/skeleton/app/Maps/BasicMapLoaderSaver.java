d58b08dbb90941be4e79c905a45c049c64a88482
Benjamin Byrkjeland <bby002@student.uib.no>
2019-03-05 23:22:30 +0100

    Added functions to parse Boolean values from saved maps

--- src/main/java/inf112/skeleton/app/Maps/BasicMapLoaderSaver.java
+++ src/main/java/inf112/skeleton/app/Maps/BasicMapLoaderSaver.java
@@ -18,8 +18,18 @@ public class BasicMapLoaderSaver implements IMapLoaderSaver {
 
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
+            int width = Integer.parseInt(bufferedReader.readLine());
+            int height = Integer.parseInt(bufferedReader.readLine());
+            Board board = new Board(width, height);
+
             while((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+                if(line == "type:Tile"){
+
+                } else if(line == "type:Laser"){
+
+                } else {
+
+                }
             }
 
             bufferedReader.close();
@@ -68,6 +78,47 @@ public class BasicMapLoaderSaver implements IMapLoaderSaver {
         }
         catch(IOException ex) {
             System.out.println("Error writing to file '" + fileName + "'");
+            return false;
+        }
+    }
+
+    public Enum parseEnum(String input){
+        return null;
+    }
+
+    /*
+    Clunky way of parsing a series of bool values from a line from a saved map.
+    Loops through the string until it finds a ':' and uses the next letter to decide it it should add true of false
+    to the walls table.
+    */
+    public Boolean[] parseWalls(String input){
+        Boolean[] walls = new Boolean[4];
+        int counter = 0;
+        for(int i = 10; i < input.length(); i++){
+            if(input.charAt(i) == ':'){
+                if(input.charAt(i+1) == 't'){
+                    walls[counter++] = true;
+                } else {
+                    walls[counter++] = false;
+                }
+            }
+        }
+        return walls;
+    }
+
+    /*
+    Clunky way of parsing a bool value from a line on a saved map.
+    For the time being the only saved bool value is isHole, and it returns false if it doesn't
+    recognize that specific substring.
+     */
+    public Boolean parseBoolean(String input){
+        if(input.substring(0, 6).equals("isHole")){
+            if(input.charAt(7) == 't'){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
             return false;
         }
     }
