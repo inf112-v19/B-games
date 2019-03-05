9d34c52fc3ef5e277661e01799407d30f37e296c
Benjamin Byrkjeland <bby002@student.uib.no>
2019-03-05 21:28:49 +0100

    Small tidying up
    -the read and write functions now returns null or false when something goes wrong
    
    -removed helping comments I didn't need anymore
    
    -added some more stuff to IMapLoaderSaver javaDoc

--- src/main/java/inf112/skeleton/app/Maps/BasicMapLoaderSaver.java
+++ src/main/java/inf112/skeleton/app/Maps/BasicMapLoaderSaver.java
@@ -11,34 +11,26 @@ public class BasicMapLoaderSaver implements IMapLoaderSaver {
     @Override
     public Board load(File map) {
 
-        // This will reference one line at a time
         String line = null;
 
         try {
-            // FileReader reads text files in the default encoding.
             FileReader fileReader = new FileReader(map);
 
-            // Always wrap FileReader in BufferedReader.
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
             while((line = bufferedReader.readLine()) != null) {
                 System.out.println(line);
             }
 
-            // Always close files.
             bufferedReader.close();
         }
         catch(FileNotFoundException ex) {
-            System.out.println(
-                    "Unable to open file '" +
-                            map + "'");
+            System.out.println("Unable to open file '" + map + "'");
+            return null;
         }
         catch(IOException ex) {
-            System.out.println(
-                    "Error reading file '"
-                            + map + "'");
-            // Or we could just do this:
-            // ex.printStackTrace();
+            System.out.println("Error reading file '" + map + "'");
+            return null;
         }
 
         return null;
@@ -72,15 +64,11 @@ public class BasicMapLoaderSaver implements IMapLoaderSaver {
             }
 
             bufferedWriter.close();
+            return true;
         }
         catch(IOException ex) {
-            System.out.println(
-                    "Error writing to file '"
-                            + fileName + "'");
-            // Or we could just do this:
-            // ex.printStackTrace();
+            System.out.println("Error writing to file '" + fileName + "'");
+            return false;
         }
-
-        return false;
     }
 }
--- src/main/java/inf112/skeleton/app/Maps/IMapLoaderSaver.java
+++ src/main/java/inf112/skeleton/app/Maps/IMapLoaderSaver.java
@@ -12,7 +12,8 @@ public interface IMapLoaderSaver {
      */
 
     /**
-     * Function to read from a text file and construct a Board object from it
+     * Function to read from a text file and construct a Board object from it.
+     * Returns null and prints an error message if it couldn't read from the specified file
      *
      * @param map the file location of the map to be loaded
      * @return a map constructed from the file
@@ -20,10 +21,12 @@ public interface IMapLoaderSaver {
     public Board load(File map);
 
     /**
-     * Function to write a map to a text file with a specified location
+     * Function to write a map to a text file with a specified location.
+     * Returns false and prints an error message if it couldn't write to the specified file
      *
      * @param map any map using the IBoard interface you want to save as a text file
      * @param fileName the file location you want to save the map to
+     * @return true if the save was successful, and false if it wasn't
      */
     public Boolean save(IBoard map, String fileName);
 }
