5ed6d704ba5e9fe67178cc41c41fff29f7f25cda
Bernhard <bhjelen2@hotmail.com>
2019-03-26 15:59:19 +0100

    Lagt til cardresolver som spiller kort etter prioritet

--- src/main/java/inf112/skeleton/app/Action.java
+++ src/main/java/inf112/skeleton/app/Action.java
@@ -12,17 +12,38 @@ public class Action implements IAction {
 
     private int roundCounter;
     private int phaseCounter;
+    private List<Player> players;
     private boolean debug = true;
     Board board;
 
     public Action(Board board){
+
         this.board = board;
     }
 
     public void updatePhase() {
+
         phaseCounter += 1;
     }
-
+    public void cardResolver() {
+        int lowestPriority = 0;
+        int indexToBePlayed = 0;
+        ArrayList<Card> CardsToBePlayed = new ArrayList<Card>();                                    //Temporary cardlist.
+        for(int i = 0; i <= players.size(); i++) {
+            CardsToBePlayed.add(players.get(i).getRegister().get(phaseCounter));                    //Fetches a card from each player to be played this phase.
+        }
+        for(int i = 0; i <= CardsToBePlayed.size(); i++) {
+            for (int j = 0; j <= CardsToBePlayed.size(); j++) {
+                if (CardsToBePlayed.get(j).getPriority() < lowestPriority) {
+                    lowestPriority = CardsToBePlayed.get(j).getPriority();
+                    indexToBePlayed = i;
+                }
+            }
+            playCard(players.get(indexToBePlayed), CardsToBePlayed.get(indexToBePlayed).getType());
+        }
+        updatePhase();
+        updateRound();
+    }
     @Override
     public void updateRound() {
         if (phaseCounter % 5 == 0 && phaseCounter != 0) {
