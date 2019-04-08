41ad1fb37e85fcfa4fac680744ef4d7203809d84
Mermersk <ic4ruz39@gmail.com>
2019-04-08 18:37:41 +0200

    Small fix, only for my branch.

--- src/main/java/inf112/skeleton/app/Action/Action.java
+++ src/main/java/inf112/skeleton/app/Action/Action.java
@@ -19,7 +19,6 @@ public class Action implements IAction {
     private int roundCounter;
     private ArrayList<Player> players;
     private int phaseCounter;
-    private ArrayList<Player> players;
     private boolean debug = true;
     Board board;
 
@@ -39,25 +38,7 @@ public class Action implements IAction {
     public void updateRound() {
         roundCounter++;
     }
-    public void cardResolver() {
-        int lowestPriority = 0;
-        int indexToBePlayed = 0;
-        ArrayList<Card> CardsToBePlayed = new ArrayList<Card>();                                    //Temporary cardlist.
-        for(int i = 0; i <= players.size(); i++) {
-            CardsToBePlayed.add(players.get(i).getRegister().get(phaseCounter));                    //Fetches a card from each player to be played this phase.
-        }
-        for(int i = 0; i <= CardsToBePlayed.size(); i++) {
-            for (int j = 0; j <= CardsToBePlayed.size(); j++) {
-                if (CardsToBePlayed.get(j).getPriority() < lowestPriority) {
-                    lowestPriority = CardsToBePlayed.get(j).getPriority();
-                    indexToBePlayed = i;
-                }
-            }
-            playCard(players.get(indexToBePlayed), CardsToBePlayed.get(indexToBePlayed).getType());
-        }
-    }
-
-
+    
     public void cardResolver() {
         int lowestPriority = 0;
         int indexToBePlayed = 0;
