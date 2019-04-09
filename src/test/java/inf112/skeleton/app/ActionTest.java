a38d62376abe3c61f6245e92a778dff1ae42299d
Jonas Bjørke Opsahl <jop004@uib.no>
2019-04-09 15:30:55 +0200

    added tests to actiontest and merged master

--- src/test/java/inf112/skeleton/app/ActionTest.java
+++ src/test/java/inf112/skeleton/app/ActionTest.java
@@ -5,6 +5,9 @@ import inf112.skeleton.app.Action.Action;
 import inf112.skeleton.app.Actor.Actor;
 import inf112.skeleton.app.Actor.Direction;
 import inf112.skeleton.app.Board.Board;
+import inf112.skeleton.app.Board.ITile;
+import inf112.skeleton.app.Board.Tile;
+import inf112.skeleton.app.Cards.Card;
 import inf112.skeleton.app.Cards.CardType;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,6 +24,13 @@ public class ActionTest {
         board = new Board();
         actor = new Actor(5, 5, Color.GREEN, board, 1);
         action = new Action(board);
+        boolean[] walls = new boolean[]{false, false, false, false};
+        ITile tile = new Tile(walls);
+        for(int i=0; i<10; i++){
+            for(int j=0; j<10; j++){
+                board.setTile(i, j, tile);
+            }
+        }
     }
 
     @Test
@@ -42,7 +52,7 @@ public class ActionTest {
     }
 
     @Test
-    public void fiveRoundUpdatesIncrementsRoundCounter(){
+    public void fiveRoundsUpdatesIncrementsRoundCounter(){
         assertEquals(action.getRound(), 0);
         for(int i=0; i<5; i++){
             action.updatePhase();
@@ -68,5 +78,73 @@ public class ActionTest {
         action.moveBackwards(actor);
         assertEquals(actor.getX(), 5);
         assertEquals(actor.getY(), 4);
+    }
+
+    @Test
+    public void rotateClockwiseRotatesPlayer(){
+        assertEquals(actor.direction, Direction.NORTH);
+        action.rotateClockwise(actor);
+        assertEquals(actor.direction, Direction.EAST);
+        action.rotateClockwise(actor);
+        assertEquals(actor.direction, Direction.SOUTH);
+    }
+
+    @Test
+    public void rotateAntiClockwiseRotatesAntiClockwise(){
+        assertEquals(actor.direction, Direction.NORTH);
+        action.rotateAntiClockwise(actor);
+        assertEquals(actor.direction, Direction.WEST);
+        action.rotateAntiClockwise(actor);
+        assertEquals(actor.direction, Direction.SOUTH);
+    }
+
+    @Test
+    public void moveCanMovePerpendicularToDirection(){
+        assertEquals(actor.getX(), 5);
+        assertEquals(actor.direction, Direction.NORTH);
+        action.move(actor, Direction.EAST);
+        assertEquals(actor.getX(), 6);
+    }
+
+    @Test
+    public void playMove1Forward(){
+        assertEquals(actor.getY(), 5);
+        action.playCard(actor, CardType.MOVE_1_FORWARD);
+        assertEquals(actor.getY(), 6);
+    }
+
+    @Test
+    public void playMove1Backward(){
+        assertEquals(actor.getY(), 5);
+        action.playCard(actor, CardType.MOVE_1_BACKWARD);
+        assertEquals(actor.getY(), 4);
+    }
+
+    @Test
+    public void playMove2Forward(){
+        assertEquals(actor.getY(), 5);
+        action.playCard(actor, CardType.MOVE_2_FORWARD);
+        assertEquals(actor.getY(), 7);
+    }
+
+    @Test
+    public void playMove3Forward(){
+        assertEquals(actor.getY(), 5);
+        action.playCard(actor, CardType.MOVE_3_FORWARD);
+        assertEquals(actor.getY(), 8);
+    }
+
+    @Test
+    public void playRotateLeft(){
+        assertEquals(actor.direction, Direction.NORTH);
+        action.playCard(actor, CardType.ROTATE_90_LEFT);
+        assertEquals(actor.direction, Direction.WEST);
+    }
+
+    @Test
+    public void playRotateRight(){
+        assertEquals(actor.direction, Direction.NORTH);
+        action.playCard(actor, CardType.ROTATE_90_RIGHT);
+        assertEquals(actor.direction, Direction.EAST);
     }
 }
