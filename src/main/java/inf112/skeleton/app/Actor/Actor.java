55288714bec416c42ffb19c30b25bb8e2e564295
Robert Frydenlund <robert_frydenlund@msn.com>
2019-04-29 17:29:53 +0200

    Added rendering for Hole and Laser, and updated player models. TextureAtlas moved to new Asset class to avoid redundant loading.

--- src/main/java/inf112/skeleton/app/Actor/Actor.java
+++ src/main/java/inf112/skeleton/app/Actor/Actor.java
@@ -5,7 +5,6 @@ import inf112.skeleton.app.Board.Board;
 import inf112.skeleton.app.Board.ITile;
 import inf112.skeleton.app.Board.Item;
 import inf112.skeleton.app.Board.RotationDirection;
-//import com.sun.xml.internal.bind.v2.TODO;
 
 public class Actor implements IActor {
     private int xPos;
